e39215f2d9bd221df78147f9a62f23141ef28e26
andjelavostic <andjelavostic@gmail.com>
2025-10-13 21:51:13 +0200

    [CHANGE] training knn model by trying out parameters

--- features/train_knn.py
+++ features/train_knn.py
@@ -21,25 +21,31 @@ scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_val_scaled   = scaler.transform(X_val)
 
-# KNN model with hyperparameter search
+# Grid search
+knn = KNeighborsClassifier()
 param_grid = {
     'n_neighbors': [3, 5, 7, 9, 11],
     'weights': ['uniform', 'distance'],
-    'p': [1, 2]  # 1=Manhattan, 2=Euclidean
+    'p': [1, 2],
+    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+    'leaf_size': [20, 30, 40]
 }
 
-knn = KNeighborsClassifier()
-grid = GridSearchCV(knn, param_grid, cv=5, scoring='accuracy', n_jobs=-1)
-grid.fit(X_train_scaled, y_train_encoded)  # fit on encoded labels
+grid = GridSearchCV(knn, param_grid, cv=5, scoring='accuracy', n_jobs=-1, verbose=2)
+grid.fit(X_train_scaled, y_train_encoded)
 
 # Best model
-print("Best KNN parameters found:", grid.best_params_)
 best_knn = grid.best_estimator_
+print("Best KNN parameters found:", grid.best_params_)
 
 # Evaluate on validation set
-y_val_pred = best_knn.predict(X_val_scaled)
-print("Validation Accuracy:", accuracy_score(y_val_encoded, y_val_pred))
-print(classification_report(y_val_encoded, y_val_pred, target_names=le.classes_))
+y_val_pred_encoded = best_knn.predict(X_val_scaled)
+
+y_val_str = le.inverse_transform(y_val_encoded) 
+y_val_pred_str = le.inverse_transform(y_val_pred_encoded)
+print("Validation Accuracy:", accuracy_score(y_val_str, y_val_pred_str))
+print(classification_report(y_val_str, y_val_pred_str, target_names=le.classes_))
+
 
 # Save model
 joblib.dump(best_knn, "../data/processed_data/knn_model.pkl")
